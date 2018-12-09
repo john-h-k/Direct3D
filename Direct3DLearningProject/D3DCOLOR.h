7b7992049259f55c3e0f3195bd9c077d14bbba05
John <johnkellyoxford@gmail.com>
2018-12-09 21:18:54 +0000

    easier packed struct
    `+color.rgba` forces color.rgba (type FLOAT[4]) to decay (to type FLOAT*), allows returning of it

--- Direct3DLearningProject/D3DCOLOR.h
+++ Direct3DLearningProject/D3DCOLOR.h
@@ -1,24 +1,21 @@
 #pragma once
-#pragma pack(push, 4)
+#pragma pack(push, 1)
 #include <windows.h>
 
 // Guaranteed to fit well in an array
 typedef struct D3DXCOLOR
 {
-	FLOAT r;
-	FLOAT g;
-	FLOAT b;
-	FLOAT a;
+	FLOAT rgba[4];
 } D3DXCOLOR;
 
 inline FLOAT* GetFloatPtrFromColor(D3DXCOLOR& color)
 {
-	return &color.r;
+	return +color.rgba;
 }
 
 inline FLOAT* GetFloatPtrFromColor(D3DXCOLOR* color)
 {
-	return &(*color).r;
+	return +(*color).rgba;
 }
 
 #pragma pack(pop)
